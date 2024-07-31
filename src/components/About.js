6ce9d963435a8d7cbc851d17d068741b3759c994
abuthecoder <abduldgl2002@gmail.com>
2024-07-31 08:03:38 +0530

    update react project

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+//About component
+function About() {
+    return (
+      <div>
+        <div className="about">
+          <h1>About Us</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quo rerum excepturi nesciunt nisi itaque, iusto odit, fugit harum aliquid, laborum id cupiditate sed illum adipisci non! Molestiae, vitae dolorem.</p>
+        </div>
+      </div>
+    )
+  }
+  export default About;
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+//search component
+function Search() {
+    return (
+      <div className="search-bar">
+        <input type="text" placeholder="Search"></input>
+      </div>
+    )
+  }
+  export default Search;
